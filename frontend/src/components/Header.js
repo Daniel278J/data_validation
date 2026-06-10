25f7a70cfd7b8ac282f82f2718ece0176b789381
Danny <gdanieljohnson27@gmail.com>
2026-06-10 13:58:18 +0530

    Fixed frontend warnings and removed logos

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,3 @@
-import logo from './images/accenture_dark.png'
 import './header.css'
 import { Link } from 'react-router-dom';
 function Header() {
@@ -6,7 +5,6 @@ function Header() {
     return (
         <Link to="/">
             <header className='App-header'>
-                <img src={logo} className="App-logo" alt="logo" />
                 <span className='Header-title'>Data Migration Tool</span>
             </header>
         </Link>
